10df9953a5fc9daeee378a2a0297b66f3cdc976f
Danila <dejaview666@yandex.ru>
2025-03-10 19:03:46 +0300

    new1

--- tests/landingPage.spec.js
+++ tests/landingPage.spec.js
@@ -4,15 +4,22 @@ test('landing page test', async ({ page }) => {
     // Открыть страницу
     await page.goto('https://polis812.github.io/vacuu/');
 
-
+    // Получаем все ссылки на странице
     const links = await page.$$eval('a', links => links.map(link => link.href));
+
+    // Проходим по всем ссылкам и проверяем их, исключая ссылки с телефоном
     for (let link of links) {
-        const response = await page.goto(link);
-        expect(response.status()).toBe(200);
+        if (!link.startsWith('tel:')) { // Игнорируем ссылки с телефонными номерами
+            const response = await page.goto(link);
+            expect(response.status()).toBe(200);
+        }
     }
+
+    // Логика для работы с элементами страницы
     const arrowElement = page.locator('span.arrow-right.arrow-enable');
     const reviewAuthor = page.locator('div.review__author', { hasText: 'Jennifer Troyer' });
+
+    // Кликаем на стрелку и проверяем, что элемент исчез
     await arrowElement.click();
     await expect(reviewAuthor).not.toBeVisible();
-
-})
+});
